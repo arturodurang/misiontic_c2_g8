789e9469c4556c3b42a8e1a1834ca4a971b3023b
ARTURO DURAN GONZALEZ <adg.estructuras@gmail.com>
2022-07-03 19:27:40 -0500

    Retos 1 y 2 C2 G8 - Calculadora

---
+++ src/main/java/c2g8adg/calculadoraretos1y2/CalculadoraRetos1y2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package c2g8adg.calculadoraretos1y2;
+
+/**
+ *
+ * @author Arturo Duran
+ */
+public class CalculadoraRetos1y2 {
+
+    public static void main(String[] args) {
+        new JFCalculadora().setVisible(true);
+    }
+}
